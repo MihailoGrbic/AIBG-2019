5174afac1b3ffd6a4225edba2f5a14315f9299d1
djokjula <djordjemarjanovic998@gmail.com>
2019-12-06 20:53:30 +0100

    Moved actions to a dictionary

--- Bot.py
+++ Bot.py
@@ -4,41 +4,42 @@ import GameState
 import Map
 import PlayerInfo
 
-class Action(enum.Enum):
+actions = {
 
     # moving
 
-    DOWN = "s"
-    UP = "w"
-    LEFT = "a"
-    RIGHT = "d"
+    "DOWN": "s",
+    "UP": "w",
+    "LEFT": "a",
+    "RIGHT": "d",
 
     # taking resources
 
-    TAKE_DOWN = "trs"
-    TAKE_UP = "trw"
-    TAKE_LEFT = "tra"
-    TAKE_RIGHT = "trd"
+    "TAKE_DOWN": "trs",
+    "TAKE_UP": "trw",
+    "TAKE_LEFT": "tra",
+    "TAKE_RIGHT": "trd",
 
     # leave resource
 
-    LEAVE_WOOD = "lw"
-    LEAVE_STONE = "lw"
-    LEAVE_METAL = "lw"
+    "LEAVE_WOOD": "lw",
+    "LEAVE_STONE": "lw",
+    "LEAVE_METAL": "lw",
 
     # sword attack
 
-    SWORD_DOWN = "sas"
-    SWORD_UP = "saw"
-    SWORD_LEFT = "saa"
-    SWORD_RIGHT = "sad"
+    "SWORD_DOWN": "sas",
+    "SWORD_UP": "saw",
+    "SWORD_LEFT": "saa",
+    "SWORD_RIGHT": "sad",
 
     # arrow attack
 
-    ARROW_DOWN = "aas"
-    ARROW_UP = "aaw"
-    ARROW_LEFT = "aaa"
-    ARROW_RIGHT = "aad"
+    "ARROW_DOWN": "aas",
+    "ARROW_UP": "aaw",
+    "ARROW_LEFT": "aaa",
+    "ARROW_RIGHT": "aad"
+}
 
 class Bot(object):
     def __init__(self, url, gameId, playerId):
