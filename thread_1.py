74edb9024f8799fb8b0149440c617907ef7a12ce
Mihailo_Grbic <mihailogrbic99@gmail.com>
2019-12-06 22:26:15 +0100

    Minor additions

--- thread_1.py
+++ thread_1.py
@@ -1,5 +1,9 @@
 from BotRandom import BotRandom
+import Client
+from BotLeft import BotLeft
+from BotKeyboard import BotKeyboard
 
-bot = BotRandom('http://localhost:9080', 0, 1)
+Client.get("http://localhost:9080/admin/createGame?gameId=0&playerOne=1&playerTwo=2&mapName=mapConfig")
+bot = BotKeyboard('http://localhost:9080', 0, 1)
 
 bot.game()
--- thread_2.py
+++ thread_2.py
@@ -1,5 +1,8 @@
-from BotKeyboard import BotKeyboard
+# from BotKeyboard import BotKeyboard
+from BotRandom import BotRandom
+from BotLeft import BotLeft
 
-bot = BotKeyboard('http://localhost:9080', 0, 2)
+# bot = BotKeyboard('http://localhost:9080', 0, 2)
+bot = BotLeft('http://localhost:9080', 0, 2)
 
 bot.game()
