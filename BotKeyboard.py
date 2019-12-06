59c9d17688c1d2921b9cf692af335fbd9224ffeb
djokjula <djordjemarjanovic998@gmail.com>
2019-12-06 21:04:04 +0100

    from bot import bot and test keyboarad

--- BotKeyboard.py
+++ BotKeyboard.py
@@ -1,4 +1,4 @@
-import Bot
+from Bot import Bot
 
 class BotKeyboard(Bot):
 
--- thread_2.py
+++ thread_2.py
@@ -1,5 +1,5 @@
-from BotRandom import BotRandom
+from BotKeyboard import BotKeyboard
 
-bot = BotRandom('http://localhost:9080', 0, 2)
+bot = BotKeyboard('http://localhost:9080', 0, 2)
 
 bot.game()
