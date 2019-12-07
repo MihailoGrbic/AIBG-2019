6ebd990febfec6672f58647cd97cddddc027a2d1
rols <znjrols5@gmail.com>
2019-12-07 12:55:31 +0100

    fix random

--- BotRandom.py
+++ BotRandom.py
@@ -1,17 +1,47 @@
 from Bot import Bot
 from Bot import actions
 import random
+from utils import move_available
 
 
 class BotRandom(Bot):
 
     def play_single_turn(self, current_game_state):
-        x = random.randint(0, 4)
-        if x == 0:
-            return actions["UP"]
-        if x == 1:
-            return actions["DOWN"]
-        if x == 2:
-            return actions["LEFT"]
-        if x == 3:
-            return actions["RIGHT"]
+        x, y = current_game_state.self_info.x, current_game_state.self_info.y
+
+        r = random.randint(0, 3)
+        if r == 0:
+            if move_available(current_game_state.map, current_game_state.other_info, x, y-1):
+                return actions["UP"]
+            else:
+                r += 1
+        if r == 1:
+            if move_available(current_game_state.map, current_game_state.other_info, x, y+1):
+                return actions["DOWN"]
+            else:
+                r += 1
+        if r == 2:
+            if move_available(current_game_state.map, current_game_state.other_info, x-1, y):
+                return actions["LEFT"]
+            else:
+                r += 1
+        if r == 3:
+            if move_available(current_game_state.map, current_game_state.other_info, x+1, y):
+                return actions["RIGHT"]
+            else:
+                r = 0
+        if r == 0:
+            if move_available(current_game_state.map, current_game_state.other_info, x, y-1):
+                return actions["UP"]
+            else:
+                r += 1
+        if r == 1:
+            if move_available(current_game_state.map, current_game_state.other_info, x, y+1):
+                return actions["DOWN"]
+            else:
+                r += 1
+        if r == 2:
+            if move_available(current_game_state.map, current_game_state.other_info, x-1, y):
+                return actions["LEFT"]
+            else:
+                r += 1
