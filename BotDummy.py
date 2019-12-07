09a3e0c519121eac6edf89a19217b877ee2042de
djokjula <djordjemarjanovic998@gmail.com>
2019-12-07 02:24:23 +0100

    Moved GameState logic

--- BotDummy.py
+++ BotDummy.py
@@ -3,8 +3,5 @@ from Bot import Bot
 
 class BotDummy(Bot):
 
-    def __init__(self, type):
-        self.type = type
-
-    def play_single_turn(self, current_game_state, current_map, self_info, other_info):
+    def play_single_turn(self, current_game_state):
         pass
