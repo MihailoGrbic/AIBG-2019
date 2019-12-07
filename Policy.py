d2316f0e8c50aa589a08542f447cfdf7cdc282a1
rols <znjrols5@gmail.com>
2019-12-07 11:02:41 +0100

    fix sum bugs

--- Policy.py
+++ Policy.py
@@ -76,12 +76,25 @@ class GetSword(Policy):
         if md is not None:
             nearest_sword = current_game_state.map.tiles[md[1]][md[0]]["item"]
             two_swords_in = max(0, 20 - nearest_sword["numWeapons"] * 10 - nearest_sword["timeSinceMakeWeapon"])
+            one_sword_in = max(0, 10 - nearest_sword["numWeapons"] * 10 - nearest_sword["timeSinceMakeWeapon"])
+        else:
+            print("GetSword False")
+            return False
 
-        ret_val = sword_fortress_exists(current_game_state) \
-            and two_swords_in <= utils.dist(md[0], md[1], current_game_state.self_info.x, current_game_state.self_info.y) - 1 \
-            and ((current_game_state.self_info.player_info["weapon1"] is None \
-            and current_game_state.self_info.player_info["weapon2"] is None) \
-            or utils.dist(md[0], md[1], current_game_state.self_info.x, current_game_state.self_info.y) == 1)
+        num_of_swords = 0
+        if current_game_state.self_info.player_info["weapon1"] is not None:
+            num_of_swords += 1
+        if current_game_state.self_info.player_info["weapon2"] is not None:
+            num_of_swords += 1
+
+        go_for_two_swords = two_swords_in <= utils.dist(md[0], md[1], current_game_state.self_info.x, current_game_state.self_info.y) - 1 \
+            and num_of_swords == 0
+
+        take_one_sword = utils.dist(md[0], md[1], current_game_state.self_info.x, current_game_state.self_info.y) == 1 \
+                         and one_sword_in == 0 \
+                         and num_of_swords == 1
+
+        ret_val = sword_fortress_exists(current_game_state) and (go_for_two_swords or take_one_sword)
 
         print("GetSword " + str(ret_val))
 
@@ -99,8 +112,10 @@ class AttackWithSword(Policy):
                                  b["itemType"] == "SWORD_FORTRESS"])
 
         num_of_swords = 0
-        if current_game_state.self_info.player_info["weapon1"] is not None: num_of_swords += 1 
-        if current_game_state.self_info.player_info["weapon2"] is not None: num_of_swords += 1 
+        if current_game_state.self_info.player_info["weapon1"] is not None:
+            num_of_swords += 1
+        if current_game_state.self_info.player_info["weapon2"] is not None:
+            num_of_swords += 1
 
         ret_val = num_of_swords == 2 or (num_of_swords == 1 and md is not None \
         and utils.dist(md[0], md[1], current_game_state.self_info.x, current_game_state.self_info.y) > 1)
