6eb23426c234eaa13f78925fe1b5d6862f4ec991
djokjula <djordjemarjanovic998@gmail.com>
2019-12-07 02:44:28 +0100

    Finished BotGetWeapon

--- BotGetWeapon.py
+++ BotGetWeapon.py
@@ -14,6 +14,8 @@ class BotGetWeapon(Bot):
         md = utils.find_nearest((current_game_state.self_info.x, current_game_state.self_info.y),
                                 [b for b in current_game_state.map.items if
                                  b["itemType"] == "SWORD_FORTRESS" and b["id"] == current_game_state.playerId])
-        self.bot_walker.x_sel = md[0]
-        self.bot_walker.y_sel = md[1]
-        self.bot_walker.play_single_turn(current_game_state)
+        if md is not None:
+            self.bot_walker.x_sel = md[0]
+            self.bot_walker.y_sel = md[1]
+            self.bot_walker.play_single_turn(current_game_state)
+        print("E, sjebao si se")
